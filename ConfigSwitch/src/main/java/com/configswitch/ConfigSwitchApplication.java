5a3ef5a94babf8d28cb78f01e195326b08cfdd77
fdigne <fdigne@me.com>
2017-10-05 10:58:15 +0200

    Version 0

---
+++ ConfigSwitch/src/main/java/com/configswitch/ConfigSwitchApplication.java
@@ -0,0 +1,12 @@
+package com.configswitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSwitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigSwitchApplication.class, args);
+	}
+}
